af34254354c59b8586038f5f04b1c38623e50261
xiaocaiji233 <54783714+xiaocaiji233@users.noreply.github.com>
2019-09-02 09:07:53 +0800

    Add files via upload

---
+++ sb.java
@@ -0,0 +1,21 @@
+package 输入;
+
+import java.util.Scanner;
+public class sb {
+
+	public static void main(String[] args) {
+int age;
+double weight;
+Scanner scanner=new Scanner(System.in);
+
+System.out.print("请问你的年龄");
+age=scanner.nextInt();
+System.out.print("请问你的体重");
+weight=scanner.nextDouble();
+
+
+
+System.out.println("你的年龄是"+age+"岁，你的体重是"+weight+"公斤");
+	}
+
+}
